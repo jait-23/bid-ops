3ad6072aaaf0cfdcac2fbac5336a5a414ce85cf7
Shweta <shweta.rewatkar2@gmail.com>
2018-04-14 15:14:13 -0700

    solicitation pg color change

--- src/main/webapp/app/entities/solicitations/solicitations.state.js
+++ src/main/webapp/app/entities/solicitations/solicitations.state.js
@@ -125,7 +125,7 @@
             views: {
             	 'appContent@home': {
                      templateUrl: 'app/entities/solicitations/solicitations-detail1.html',
-                     controller: 'SolicitationsDetail1Controller',
+                     controller: 'SolicitationsDetailController',
                      controllerAs: 'vm'
             	 }
             },
