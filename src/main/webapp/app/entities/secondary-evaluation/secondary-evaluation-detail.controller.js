5e6f1f93089a01f9d67e48e72f7c89d4f8b069de
kasturi ghonsikar <k.ghonsikar@gmail.com>
2018-05-06 22:02:21 -0700

    Fee button, controller in process- (Tej)

--- src/main/webapp/app/entities/secondary-evaluation/secondary-evaluation-detail.controller.js
+++ src/main/webapp/app/entities/secondary-evaluation/secondary-evaluation-detail.controller.js
@@ -13,20 +13,7 @@
         vm.secondaryEvaluation = entity;
         vm.previousState = previousState.name;
         
-        console.log(vm.secondaryEvaluation);
-        var total=0;
-        var count=0;
-        for(i=0; i<vm.secondaryEvaluation.length[i]; i++) {
-        	if(vm.secondaryEvaluation[i].score!="null" && vm.secondaryEvaluation[i].score!=undefined)
-        		{
-        	      total+=vm.secondaryEvaluation[i].score;
-        	      count++;
-        		}
-        }
-        
-        for(i=0; i<vm.secondaryEvaluation.length[i]; i++){
-        	
-        }
+      
 
         var unsubscribe = $rootScope.$on('bidopscoreApp:secondaryEvaluationUpdate', function(event, result) {
             vm.secondaryEvaluation = result;
