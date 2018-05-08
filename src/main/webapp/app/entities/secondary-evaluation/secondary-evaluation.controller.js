afda54603ff9e656975a7f0b5e09ebd18b483286
jait23 <jait@si2tech.com>
2018-05-07 23:33:01 -0700

    secondary evaluations average count

--- src/main/webapp/app/entities/secondary-evaluation/secondary-evaluation.controller.js
+++ src/main/webapp/app/entities/secondary-evaluation/secondary-evaluation.controller.js
@@ -12,7 +12,19 @@
         var vm = this;
         console.log(vm);
     	vm.bidders = Bidders.query();
+    	$scope.secondaryEvaluation = SecondaryEvaluation.query();
+    	
+    	SecondaryEvaluation.query().$promise
+		.then(function(result) {
+			console.log("promised SecondaryEvaluation");
+			console.log(result);
+			$scope.totalCount();
+		});
+    	console.log($scope.secondaryEvaluation);
     	console.log(vm.bidders);
+    	
+    	$scope.secondaryEvaluationTotal = [];
+		$scope.secondaryEvaluation = [];
 
         vm.loadPage = loadPage;
         vm.predicate = pagingParams.predicate;
@@ -34,6 +46,7 @@
                     result.push('id');
                 }
                 return result;
+                totalAvgScore();
             }
             function onSuccess(data, headers) {
                 vm.links = ParseLinks.parse(headers('link'));
@@ -60,21 +73,23 @@
             });
         }
         
-        /*console.log(vm.secondaryEvaluations);
-        var total=0;
-        var count=0;
+        $scope.total=0;
+        $scope.count=0;
         
-        vm.secondaryEvaluations.forEach( function (arrayItem)
-        	{
-        	if(vm.secondaryEvaluations[i].score!="null" && vm.secondaryEvaluations[i].score!=undefined)
-    		{
-    	      total+=vm.secondaryEvaluations[i].score;
-    	      console.log(total);
-    	      count++;
-    	      console.log(count);
-    		}});
         
-        var average=total/count;
+       // console.log(totalAvgScore());
+        
+      $scope.totalCount = function(){
+    	  for (var i=0; i < $scope.secondaryEvaluation.length; i++) {
+        		if ($scope.secondaryEvaluation[i].score != null &&  $scope.secondaryEvaluation[i].score != undefined && $scope.secondaryEvaluation[i].score != 'null') {
+        			$scope.total += $scope.secondaryEvaluation[i].score;
+        			console.log($scope.total);
+        			console.log($scope.count);
+        		}
+        	}
+      }
+        
+        /*var average=total/count;
         
         vm.secondaryEvaluations.forEach( function (arrayItem)
             	{
@@ -86,8 +101,8 @@
         		vm.secondaryEvaluations[i].eligible="no";
         		console.log("It was a NO");
         	}
-        		});*/
-        /*for(var i=0; i<vm.secondaryEvaluations.length; i++) {
+        		});
+        for(var i=0; i<vm.secondaryEvaluations.length; i++) {
         	if(vm.secondaryEvaluations[i].score!="null" && vm.secondaryEvaluations[i].score!=undefined)
         		{
         	      total+=vm.secondaryEvaluations[i].score;
@@ -110,7 +125,7 @@
         	}
         	
 	
-        }*/
+        } */
         
         $scope.showFee = function(userId) {
         	
